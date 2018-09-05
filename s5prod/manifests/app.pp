059c35986bc74043fce373462fefda251fb3a514
Aditya <sp.aditya@gmail.com>
2018-09-05 15:31:37 +0530

    Update app.pp

--- s5prod/manifests/app.pp
+++ s5prod/manifests/app.pp
@@ -5,21 +5,21 @@ node /nodep[1-9]/{
   	exec { 'apt-get update':
     		path=>['/usr/bin/'] }
   	class {'apache2': apache_port => $apache_port }
-        package{'git':}->
-	vcsrepo { '/tmp/mycode':
-  		ensure     => latest,
-  		provider   => git,
-  		source     => 'https://github.com/AdityaSP/puppetcode',
-	}->
-        file{'/var/www/html/index.html':
-              source => "/tmp/mycode/index.html"
-        }
+#        package{'git':}->
+#	vcsrepo { '/tmp/mycode':
+#  		ensure     => latest,
+#  		provider   => git,
+#  		source     => 'https://github.com/AdityaSP/puppetcode',
+#	}->
+#        file{'/var/www/html/index.html':
+#              source => "/tmp/mycode/index.html"
+#        }
 
   	Exec['apt-get update']->
   	Package['apache2'] -> File['/etc/apache2/ports.conf'] -> Service['apache2']
   	File['/etc/apache2/ports.conf'] ~> Service['apache2']
 
-        Package['apache2'] -> File['/var/www/html/index.html']
+ #       Package['apache2'] -> File['/var/www/html/index.html']
   }
   if $::boxrole == 'db' {
 	class { '::mysql::server': }
